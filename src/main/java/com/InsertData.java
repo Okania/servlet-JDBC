78a099c28b6d48ae93f209c8d88ada1e6f885df5
Okania <evans.okania@strathmore.edu>
2023-01-08 18:16:38 +0300

    new commits

--- src/main/java/com/InsertData.java
+++ src/main/java/com/InsertData.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//import DatabaseConnection;
 
-@WebServlet("/insert")
+
+//@WebServlet("/insert")
 public class InsertData extends HttpServlet{
 	/**
 	 * 
@@ -20,24 +23,32 @@ public class InsertData extends HttpServlet{
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException{
+		res.setContentType("text/html");
 		try {
 			Connection con = DatabaseConnection.initDatabase();
-			PreparedStatement st = con.prepareStatement("insert into tbl_users values(?,?,?,?)");
+			PreparedStatement st = con.prepareStatement("insert into details values(?,?)");
 			
-			st.setInt(1, Integer.valueOf(req.getParameter("uid")));
-			st.setString(2, req.getParameter("fname"));
-			st.setString(3, req.getParameter("lname"));
-			st.setInt(4, Integer.valueOf(req.getParameter("age")));
+//			st.setInt(1, Integer.valueOf(req.getParameter("uid")));
+			st.setString(1, req.getParameter("fname"));
+			st.setString(2, req.getParameter("lname"));
+//			st.setInt(4, Integer.valueOf(req.getParameter("age")));
 			
-			st.executeQuery();
+			st.executeUpdate();
 			
 			st.close();
 			con.close();
 			PrintWriter pw = res.getWriter();
 			
-			pw.println("<html><body><b>Successfully Inserted" + "</b></body></html>");		
+			pw.println("<h1>" + "DATA INSERTED SUCCESSFULLY!" + "</h1>");		
 			
-		} catch(Exception e) {
+		} catch(ClassNotFoundException e) {
+			PrintWriter writer = res.getWriter();
+			writer.println("<h1>" + "MySQL Driver class not found!" + "</h1>");
+			e.printStackTrace();
+			
+		} catch(SQLException e) {
+			PrintWriter wr = res.getWriter();
+			wr.println("<h1>" + "Error while executing Query!" + "</h1>");
 			e.printStackTrace();
 		}
 	}	
